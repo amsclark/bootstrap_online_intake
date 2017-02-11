d77c5fc1285030de3392f14d778105f4975c0e46
Alex Clark <amsclark@gmail.com>
2017-02-11 15:19:15 -0600

    working on building out gen.html

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ echo "\t<title>Legal Aid of Nebraska | Application for Services </title>\n";
 echo "\t<link href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u\" crossorigin=\"anonymous\">\n";
 echo "\t<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js\"></script>\n";
 echo "\t<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\" integrity=\"sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa\" crossorigin=\"anonymous\"></script>\n";
+echo "\t<script src=\"js/validator.js\"></script>\n";
 echo "</head>\n";
 
 echo "<body>\n";
